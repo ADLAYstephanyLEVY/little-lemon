6283c47063e06dfc4f7cb3248877d6f64cce4cf6
Adlay Levy <adlyfany@gmail.com>
2025-01-28 12:59:24 -0600

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        display: "flex",
+        gap: "1rem",
+        justifyContent: "center",
+        backgroundColor:'#495E57'
+      }}
+    >
+      <div>image</div>
+      <div>
+        Doormat navigation
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        Social Media Links
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        Contact info
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
