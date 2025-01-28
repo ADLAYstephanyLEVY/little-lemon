4299fe0fd739f141948bda9857cae258c0235952
Adlay Levy <adlyfany@gmail.com>
2025-01-28 12:59:12 -0600

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <meta name="description" content="Little Lemon Restaurant" />
+      <meta name="og:title" content=""/>
+      <meta name="og:description" content=""/>
+      <meta name="og:image" content=""/>
+    </header>
+  );
+}
